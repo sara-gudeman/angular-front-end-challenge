fe17d6dbf2884bb54a7cd4071bd7b540a8567c2d
Sara Gudeman <saragudeman@Saras-MacBook-Pro.local>
2016-02-28 22:19:32 -0800

    (fix) nav bar shows/hides for larger screens correctly

--- app/nav.directive.js
+++ app/nav.directive.js
@@ -7,11 +7,9 @@ function bcNavbar () {
     restrict: 'E',
     templateUrl: 'navbar.template.html',
     controller: function($scope) {
-      $scope.showNav = false;
-      $scope.darkenBg = false;
+      $scope.isHidden = false;
       $scope.toggle = function() {
-        $scope.showNav = !$scope.showNav;
-        $scope.darkenBg = !$scope.darkenBg;
+        $scope.isHidden = !$scope.isHidden;
       };
     }
   };
@@ -22,22 +20,22 @@ angular
   .run(function($templateCache) {
   $templateCache.put('navbar.template.html',
     '<header class="top-bar shadow">' +
-      '<svg ng-show="showNav" ng-click="toggle()" class="menu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">' +
+      '<svg ng-class="{\'hide\': !isHidden}" ng-click="toggle()" class="menu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">' +
         '<path d="M19 6.41l-1.41-1.41-5.59 5.59-5.59-5.59-1.41 1.41 5.59 5.59-5.59 5.59 1.41 1.41 5.59-5.59 5.59 5.59 1.41-1.41-5.59-5.59z"/>' +
         '<path d="M0 0h24v24h-24z" fill="none"/>' +
       '</svg>'+
-      '<svg ng-show="!showNav" ng-click="toggle()" class="menu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">'+ 
+      '<svg ng-class="{\'hide\': isHidden}" ng-click="toggle()" class="menu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">'+ 
         '<path d="M0 0h24v24h-24z" fill="none"/>'+
         '<path d="M3 18h18v-2h-18v2zm0-5h18v-2h-18v2zm0-7v2h18v-2h-18z"/>'+
       '</svg>'+
       '<h2 class="head-title">' +
         '{{header}}' +
       '</h2>' +
-      '<nav ng-show="showNav" class="top-bar">' +
-        '<ul>'+
-          '<li ng-repeat="nav in navOptions">{{nav}}</li>' +
-        '</ul>' +
-      '</nav>'+
     '</header>'+
-    '<div class="darken-bg" ng-show="darkenBg"></div>');
+    '<nav ng-class="{\'hide\': !isHidden}" class="top-bar">' +
+      '<ul>'+
+        '<li ng-repeat="nav in navOptions">{{nav}}</li>' +
+      '</ul>' +
+    '</nav>'+
+    '<div class="darken-bg" ng-class="{\'hide\': !isHidden}"></div>');
 });
