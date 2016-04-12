37eb6c673f7df54df96c81370e649bf58b7802e5
Sara Gudeman <sgudeman982@gmail.com>
2016-04-11 20:33:23 -0700

    (refactor) changes hamburger menu to svg bg as per suggestion

--- app/nav/nav.directive.js
+++ app/nav/nav.directive.js
@@ -12,8 +12,10 @@ function bcNavbar () {
     templateUrl: 'navbar.template.html',
     controller: function($scope) {
       $scope.isHidden = false;
+      $scope.background = 'svg-ic_open_menu';
       $scope.toggle = function() {
         $scope.isHidden = !$scope.isHidden;
+        $scope.background = $scope.isHidden ? 'svg-ic_close_menu' : 'svg-ic_open_menu';
       };
     }
   };
@@ -24,14 +26,7 @@ angular
   .run(function($templateCache) {
   $templateCache.put('navbar.template.html',
     '<header class="top-bar shadow">' +
-      '<svg ng-class="{\'hide\': !isHidden}" ng-click="toggle()" class="menu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">' +
-        '<path d="M19 6.41l-1.41-1.41-5.59 5.59-5.59-5.59-1.41 1.41 5.59 5.59-5.59 5.59 1.41 1.41 5.59-5.59 5.59 5.59 1.41-1.41-5.59-5.59z"/>' +
-        '<path d="M0 0h24v24h-24z" fill="none"/>' +
-      '</svg>'+
-      '<svg ng-class="{\'hide\': isHidden}" ng-click="toggle()" class="menu" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">'+ 
-        '<path d="M0 0h24v24h-24z" fill="none"/>'+
-        '<path d="M3 18h18v-2h-18v2zm0-5h18v-2h-18v2zm0-7v2h18v-2h-18z"/>'+
-      '</svg>'+
+      '<button class="menu-btn" ng-class="background" ng-click="toggle()"></button>'+
       '<h2 class="head-title">' +
         '{{title}}' +
       '</h2>' +
